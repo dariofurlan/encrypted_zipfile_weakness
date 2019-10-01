cc028bab8b85af5d8baf2dff597a6885242417b8
Dario Furlan <dario.furlan.51@gmail.com>
2019-10-01 15:57:35 +0200

    updates

--- start.py
+++ start.py
@@ -8,17 +8,16 @@ for n1 in range(0, 10):
         for n3 in range(0, 10):
             for n4 in range(0, 10):
                 guess = ''.join(map(str, [n1, n2, n3, n4]))
-                for zip_file in files:
-                    password = None
-                    with ZipFile(zip_file) as zf:
-                        try:
-                            zf.extractall(pwd=bytes(guess, 'utf-8'))
-                        except:
-                            pass
-                            # print("Tried Unsuccessfully: %s" % guess)
-                        else:
-                            print("File: %s  Password: %s" % (zip_file, guess))
-                            password = guess
-                            break
-                    if password is not None:
-                        ZipFile(zip_file).extractall(pwd=bytes(password, 'utf-8'))
+                for n in range(len(files)):
+                    zip_file = files[n]
+                    if zip_file is not None:
+                        password = None
+                        with ZipFile(zip_file) as zf:
+                            try:
+                                zf.setpassword(bytes(guess, 'utf-8'))
+                                if zf.testzip() is None:
+                                    print("File: %s  Password: %s" % (zip_file, guess))
+                                    password = guess
+                                    files[n] = None
+                            except:
+                                pass
